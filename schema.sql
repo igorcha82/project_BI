7a61b303ef373e6878c978c24a998ab4a89bbb8e
igorcha82 <67188317+igorcha82@users.noreply.github.com>
2020-12-06 13:16:35 +0300

    06/12

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS Organization (
     id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    inn        INTEGER NOT NULL     COMMENT 'ИНН',
-    kpp        INTEGER NOT NULL     COMMENT 'КПП',
+    inn        VARCHAR(20) NOT NULL     COMMENT 'ИНН',
+    kpp        VARCHAR(20) NOT NULL     COMMENT 'КПП',
     name       VARCHAR(20) NOT NULL COMMENT 'Название',
     fullname   VARCHAR(100) NOT NULL COMMENT 'Полное название',
     address    VARCHAR(50) NOT NULL COMMENT 'Адрес',
@@ -13,7 +13,7 @@ COMMENT ON TABLE Organization IS 'Организация';
 CREATE TABLE IF NOT EXISTS Office (
     id          INTEGER              COMMENT 'Id офиса',
     org_id      INTEGER NOT NULL     COMMENT 'Id организации',
-    name        INTEGER NOT NULL     COMMENT 'Номер офиса',
+    name        VARCHAR(16) NOT NULL     COMMENT 'Номер офиса',
     phone       VARCHAR(16)          COMMENT 'Телефон офиса',
     address     VARCHAR(50)          COMMENT 'Адрес офиса',
     is_active   BOOL                 COMMENT 'Статус офиса',
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS Document (
 COMMENT ON TABLE Document IS 'Документ';
 
 
-CREATE TABLE IF NOT EXISTS UserDocument (
+CREATE TABLE IF NOT EXISTS Document_Type (
     id            INTEGER      NOT NULL COMMENT 'порядковый Id документа' PRIMARY KEY,
     code          VARCHAR(20)  NOT NULL COMMENT 'Код документа',
     name          VARCHAR(50)  NOT NULL COMMENT 'Наименование документа',
@@ -52,10 +52,10 @@ CREATE TABLE IF NOT EXISTS UserDocument (
 COMMENT ON TABLE UserDocument IS 'join-таблица для связи пользователя и документа';
 
 
-CREATE TABLE IF NOT EXISTS GuidCountry (
+CREATE TABLE IF NOT EXISTS Countres (
     id            INTEGER      NOT NULL COMMENT 'порядковый Id ' PRIMARY KEY,    
     name          VARCHAR(20) NOT NULL COMMENT 'Наименование страны',
-    code          INTEGER     NOT NULL COMMENT 'Код страны'
+    code          VARCHAR(20)     NOT NULL COMMENT 'Код страны'
 );
 COMMENT ON TABLE GuidCountry IS 'Справочник стран';
 
@@ -68,7 +68,7 @@ ALTER TABLE User ADD CONSTRAINT company_id FOREIGN KEY (org_id) REFERENCES Organ
 CREATE INDEX IX_DocumentUser_Id ON Document(user_id);
 ALTER TABLE Document ADD FOREIGN KEY (user_id) REFERENCES User(id);
 
-CREATE INDEX IX_DocumentType_Id ON UserDocument(document_type);
-ALTER TABLE UserDocument ADD FOREIGN KEY (document_type) REFERENCES Document(id);
+CREATE INDEX IX_DocumentType_Id ON Document_Type(document_type);
+ALTER TABLE Document_Type ADD FOREIGN KEY (document_type) REFERENCES Document(id);
 
 
